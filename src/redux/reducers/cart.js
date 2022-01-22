8ab57f7e99e02fde7b2b7c2fad1d6988b7cd1ba5
phuquang147 <phuquang14722@gmail.com>
2022-01-22 09:16:48 +0700

    Setup redux for Cart

---
+++ src/redux/reducers/cart.js
@@ -0,0 +1,78 @@
+import { ADD_PRODUCT, UPDATE_PRODUCT, UPDATE_TOTALPRICES, DELETE_PRODUCT } from "../constants/cart";
+
+const initialState = {
+    selectedProducts: [
+        {
+            id: 1,
+            name: 'Tuổi trẻ đáng giá bao nhiêu',
+            price: 30000,
+            quantities: 1
+        },
+        {
+            id: 2,
+            name: 'Người thành công làm gì vào buổi tối',
+            price: 24000,
+            quantities: 4
+        },
+        {
+            id: 3,
+            name: 'Tuổi trẻ đáng giá bao nhiêu',
+            price: 17000,
+            quantities: 4
+        },
+        {
+            id: 4,
+            name: 'Tuổi trẻ đáng giá bao nhiêu',
+            price: 20000,
+            quantities: 4
+        },
+        {
+            id: 5,
+            name: 'Tuổi trẻ đáng giá bao nhiêu',
+            price: 50000,
+            quantities: 4
+        },
+        {
+            id: 6,
+            name: 'Tuổi trẻ đáng giá bao nhiêu',
+            price: 10000,
+            quantities: 4
+        }
+    ],
+    totalPrices: 0
+}
+
+const cartReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_PRODUCT:
+            return {
+                ...state,
+                selectedProducts: [...state.selectedProducts, action.payload]
+            }
+        case UPDATE_PRODUCT:
+            const index = state.selectedProducts.findIndex(product => product.id === action.payload.id)
+            const newProductList = [...state.selectedProducts]
+            newProductList[index] = action.payload
+            const newTotalPrices = newProductList.reduce((accumulator, item) => accumulator + item.price * item.quantities, 0)
+            return {
+                ...state,
+                selectedProducts: newProductList,
+                totalPrices: newTotalPrices
+            }
+        case UPDATE_TOTALPRICES:
+            return {
+                ...state,
+                totalPrices: action.payload
+            }
+        case DELETE_PRODUCT:
+            const newProducts = state.selectedProducts.filter(product => product.id !== action.payload.id)
+            return {
+                ...state,
+                selectedProducts: newProducts
+            }
+        default:
+            return state
+    }
+}
+
+export default cartReducer;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import exampleReducer from './example';
+import cartReducer from './cart';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-    example: exampleReducer
+    example: exampleReducer,
+    cart: cartReducer
 })
 
 export default rootReducer;
