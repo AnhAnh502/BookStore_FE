dbf03348381067318a9d70d785f10ad674c4e6be
phuquang147 <phuquang14722@gmail.com>
2022-01-23 13:17:11 +0700

    Setup redux for user

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,24 @@
+import { SET_USER, DELETE_USER } from '../constants/user'
+
+const initialState = {
+    name: '',
+    token: ''
+}
+
+const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER:
+            return {
+                ...action.payload
+            }
+        case DELETE_USER:
+            return {
+                name: '',
+                token: ''
+            }
+        default :
+            return state
+    }
+}
+
+export default userReducer;
