05cca87327b26e91b79e33e74fd8ec7484d21090
dnau6tm <97821449+dnau6tm@users.noreply.github.com>
2022-01-23 16:55:51 +0700

    add responsive, install axios and setup axios baseURL

---
+++ src/api/contacts.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "http://167.71.211.204"
+})
--- src/components/detailOfBook/Detail.jsx
+++ src/components/detailOfBook/Detail.jsx
@@ -2,37 +2,37 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from "react";
 import Header from '../common/Header/Header';
-// import axios from '../../api/contacts';
+import axios from '../../api/contacts';
 import './detail.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DATA from '../../Data';
+import { useParams } from "react-router-dom";
+
 
 
 const Detail = () => {
+  // let {id} = useParams();
 
     const [product, setProduct] = useState({});
     const [number,setNumber] = useState(1);
 
-    useEffect(()=>{
-      const getBook = DATA.find(function (e) {
-        return e.id === 0;
-      })
-      setProduct(getBook);
-    })
+    
+    const callBookApi = async () => {
+        const response = await axios.get("/api/v1/books")
+        return response.data;
+      };
 
-    // const callBookApi = async () => {
-    //     const response = await axios.get("/api/books")
-    //     return response.data;
-    //   };
-
-      // useEffect(() => {
-      //   const getBooks = async (id) => {
-      //     const Books = await callBookApi();
-      //     const book = Books[id];
-      //     if(book) setProduct(book);
-      // }
-      // getBooks();
-      // }, []);
+      useEffect(() => {
+        const getBooks = async () => {
+          const Books = await callBookApi();
+          console.log(Books);
+          const book = Books.products.find((e) => {
+           return  e._id === "61eab6ebaa5731d9327fa0cf"
+          })
+          console.log(book)
+          if(book) setProduct(book);
+      }
+      getBooks();
+      }, []);
 
       const handleIncrease = () => {
         setNumber(number + 1)
@@ -43,10 +43,10 @@ const Detail = () => {
       }
 
       const postToCart = async () => {
-        // let dataToCart = await axios.post("/api/cart",{
-        //   name:product.name,
-        //   number: number
-        // })
+        await axios.post("/api/v1/order",{
+          bookId: product._id,
+          quantity: number
+        })
         
       }
 
@@ -54,21 +54,22 @@ const Detail = () => {
       <>
         <Header />
         <div class="container">
-          <div class="col-lg-12 border p-3 main-section bg-white m-auto mt-5">
+          <div class="col-lg-12 border p-3 main-section bg-white mt-5">
               <div class="row m-5">
                   <div class="col-lg-4 left-side-product-box pb-3">
-                      <img src={product.img} class="border p-3" width="100%" height="100%" />
-                      <p>Nhà xuất bản: {product.publisher}</p>
-                      <p>Xuất bản ngày: {product.published}</p>
+                      <img src={product.imageUrl} class="border p-3" width="100%" height="100%" />
+                      <div className="">
+                        <div className='sm h6'>Nhà xuất bản: {product.publisher}</div>
+                        <div className='sm h6'>Xuất bản ngày: {product.published}</div>
+                      </div>
                   </div>
-                  <div class="col-lg-8">
-                      <div class="right-side-pro-detail border p-3 m-0">
+                  <div class="col-lg-8 mt-5">
+                      <div class="right-side-pro-detail p-2 pt-4">
                           <div class="row">
                               <div class="col-lg-12">
                                   <span className="h4">Tên sách</span>
-                                  <p class="m-0 p-0 d-flex">
-                                    <p className='display-4'>{product.name}</p>
-                                    <h5 className='mt-4'> - {product.title}</h5>
+                                  <p class="m-0 p-0 ">
+                                    <p className='display-4'>{product.title}</p>
                                   </p>
                                   <p>{product.author}</p>
 
@@ -79,7 +80,7 @@ const Detail = () => {
                               </div>
                               <div class="col-lg-12 pt-2">
                                   <h5>Mô tả cuốn sách</h5>
-                                  <span>{product.desc}</span>
+                                  <span>{product.description}</span>
                                   <hr class="m-0 pt-2 mt-2"/>
                               </div>
                               <div class="col-lg-12">
