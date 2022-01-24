e58c88b8bb3e3d0888ec942c76f4ed376b6c29b5
dnau6tm <97821449+dnau6tm@users.noreply.github.com>
2022-01-24 21:41:07 +0700

    change img slide

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,16 +17,16 @@ export default function Home() {
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="/assets/images/home/img1.jpg" class="d-block w-100" alt="Book" height="500px"/>
+                        <img src="https://media.istockphoto.com/photos/books-picture-id949118068?b=1&k=20&m=949118068&s=170667a&w=0&h=YZ_FOpR300sPowTqW0f0NaebJx2KfQvGxbafAIsfALA=" class="d-block w-100" alt="Book" height="500px"/>
                 </div>
                         <div class="carousel-item">
-                            <img src="/assets/images/home/img2.jpg" class="d-block w-100" alt="Book" height="500px"/>
+                            <img src="https://cdn.pixabay.com/photo/2021/01/21/15/54/books-5937716__340.jpg" class="d-block w-100" alt="Book" height="500px"/>
                 </div>
                             <div class="carousel-item">
-                                <img src="/assets/images/home/img3.jpg" class="d-block w-100" alt="Book" height="500px"/>
+                                <img src="https://cdn.pixabay.com/photo/2016/03/26/22/21/books-1281581__340.jpgg" class="d-block w-100" alt="Book" height="500px"/>
                 </div>
                             <div class="carousel-item">
-                                <img src="/assets/images/home/img4.jpg" class="d-block w-100" alt="Book" height="500px"/>
+                                <img src="https://cdn.pixabay.com/photo/2016/02/16/21/07/books-1204029__340.jpg" class="d-block w-100" alt="Book" height="500px"/>
                 </div>
                             </div>
                             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
