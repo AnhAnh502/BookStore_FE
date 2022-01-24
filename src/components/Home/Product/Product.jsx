d930044829c3d0ca0c0789e681e5e41f6317202f
dnau6tm <97821449+dnau6tm@users.noreply.github.com>
2022-01-24 21:53:58 +0700

    add home/product

--- src/components/Home/Product/Product.jsx
+++ src/components/Home/Product/Product.jsx
@@ -1,49 +1,61 @@
-import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
-import { applyMiddleware } from "redux";
-import axios from "../../../api/contacts";
+import React, { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom';
+// import { LinkContainer } from 'react-router-bootstrap';
+import { Container} from 'react-bootstrap';
+// import DATA from '../Data'
+import { axiosInstance } from '../../../apis/baseApi';
+// import '../../../'
+
+
 
 const Product = () => {
-    
-    const getDatabooks = async () => {
-        const response = await axios.get("/api/books");
+    const [productlist,setProductList] = useState([]);
+    const callBookApi = async () => {
+        const response = await axiosInstance.get("/books")
+        console.log(response.data)
         return response.data;
-    }
-
-    const [books,setBooks] = useState([]);
+      };
     
-    useEffect(()=>{
-        const getBooks = async () => {
-            const Books = await getDatabooks();
-            if(Books) setBooks(Books);
-        }
-        getBooks();
-    },[])
+      useEffect(() => {
+        const getProducts = async () => {
+          const Books = await callBookApi();
+          if(Books) setProductList(Books.products);
+          console.log(productlist);
+      }
+      getProducts();
+      }, []);
     
-    console.log(books);
-
-    const addDataBook = () => {
-
+    const cardItem = (item) => {
+        return (
+            <Container className="card my-5 py-4" key={item._id} style={{width: "18rem"}}>
+                <img src={item.imageUrl} className="card-img-top" alt={item.title}/>
+                    <div className="card-body text-center">
+                        <h5 className="card-title">{item.title}</h5>
+                        <p className="lead">{item.price} VNĐ</p>
+                        <NavLink to={`/products/${item._id}`} className="btn btn-primary">Chi Tiết</NavLink>
+                    </div>
+            </Container>
+                );
     }
-
-
-    return (
-        <>
-            <ul className="productList">
-                {books.map((book,index) =>{
-                    console.log(book.name);
-                    return (
-                        <li key={book.id}
-                        >
-                            <a href={`/products/${book.id}`}>
-                                {book.name}
-                            </a>
-                        </li>
-                    )
-                })}
-            </ul>
-        </>
-    )
+                return (
+                <Container>
+                    <div className="container py-5">
+                        <div className="row">
+                            <div className="col-12 text-center">
+                                <h1>Danh sách sản phẩm</h1>
+                                <hr />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container">
+                        <div className="row justify-content-around">
+                            {productlist.map(cardItem)}
+                        </div>
+                    </div>
+                </Container>
+                )
 }
 
-export default Product;
+                export default Product
+
+>>>>>>> Stashed changes
